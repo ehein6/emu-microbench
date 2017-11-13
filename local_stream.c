2c20f7e04415129121348a10b0f21d9a94e32bc3
Eric Hein <ehein6@gatech.edu>
2017-11-13 15:28:11 -0500

    Removed copy_ benchmarks.

--- local_stream.c
+++ local_stream.c
@@ -43,26 +43,10 @@ local_stream_deinit(local_stream_data * data)
 }
 
 void
-local_stream_copy_serial(local_stream_data * data)
-{
-    for (long i = 0; i < data->n; ++i) {
-        data->c[i] = data->a[i];
-    }
-}
-
-void
 local_stream_add_serial(local_stream_data * data)
 {
     for (long i = 0; i < data->n; ++i) {
         data->c[i] = data->a[i] + data->b[i];
-    }
-}
-
-void
-local_stream_copy_cilk_for(local_stream_data * data)
-{
-    cilk_for (long i = 0; i < data->n; ++i) {
-        data->c[i] = data->a[i];
     }
 }
 
