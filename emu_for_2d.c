d86f0d59cf6d49399650be9cfb6cb38941d65ead
Eric Hein <ehein6@gatech.edu>
2018-01-12 15:41:09 -0500

    Auto-generate loop templates using python.
    Update with `python -m cogapp -r emu_for_2d.c emu_for_2d.h`

--- emu_for_2d.c
+++ emu_for_2d.c
@@ -10,10 +10,52 @@
 #define noinline __attribute__ ((noinline))
 #endif
 
-// Zero-argument version
 
+/*[[[cog
+
+from string import Template
+
+for num_args in xrange(6):
+
+    arg_decls = "".join([", void * arg%i"%(i+1) for i in xrange(num_args)])
+    arg_list = "".join([", arg%i"%(i+1) for i in xrange(num_args)])
+
+    functions=Template("""
+        static noinline void
+        emu_chunked_array_apply_v${num_args}_level1(void * hint, long begin, long end, long grain
+            ${arg_decls},
+            void (*worker)(long begin, long end${arg_decls})
+        )
+        {
+            (void)hint;
+            for (long i = begin; i < end; i += grain) {
+                long first = i;
+                long last = first + grain; if (last > end) { last = end; }
+                cilk_spawn worker(first, last${arg_list});
+            }
+        }
+
+        void
+        emu_chunked_array_apply_v${num_args}(void ** array, long n, long grain
+            ${arg_decls},
+            void (*worker)(long begin, long end${arg_decls}))
+        {
+            // Each thread will be responsible for the elements on one nodelet
+            long local_n = n / NODELETS();
+            // Spawn a thread on each nodelet
+            for (long i = 0; i < NODELETS(); ++i) {
+                long begin = local_n * i;
+                long end = local_n * (i + 1); if (end > n) { end = n; }
+                cilk_spawn emu_chunked_array_apply_v${num_args}_level1(array[i], begin, end, grain${arg_list}, worker);
+            }
+        }
+
+    """)
+    cog.out(functions.substitute(**locals()), dedent=True, trimblanklines=True)
+]]]*/
 static noinline void
-emu_chunked_array_apply_v0_level1(void * hint, long begin, long end, long grain,
+emu_chunked_array_apply_v0_level1(void * hint, long begin, long end, long grain
+    ,
     void (*worker)(long begin, long end)
 )
 {
@@ -25,9 +67,9 @@ emu_chunked_array_apply_v0_level1(void * hint, long begin, long end, long grain,
     }
 }
 
-// Assumes array is an array allocated with mw_malloc2d(NODELETS(), element_size * n/NODELETS())
 void
-emu_chunked_array_apply_v0(void ** array, long n, long grain,
+emu_chunked_array_apply_v0(void ** array, long n, long grain
+    ,
     void (*worker)(long begin, long end))
 {
     // Each thread will be responsible for the elements on one nodelet
@@ -40,11 +82,9 @@ emu_chunked_array_apply_v0(void ** array, long n, long grain,
     }
 }
 
-// One-argument version
-
 static noinline void
-emu_chunked_array_apply_v1_level1(void * hint, long begin, long end, long grain,
-    void * arg1,
+emu_chunked_array_apply_v1_level1(void * hint, long begin, long end, long grain
+    , void * arg1,
     void (*worker)(long begin, long end, void * arg1)
 )
 {
@@ -56,10 +96,9 @@ emu_chunked_array_apply_v1_level1(void * hint, long begin, long end, long grain,
     }
 }
 
-// Assumes array is an array allocated with mw_malloc2d(NODELETS(), element_size * n/NODELETS())
 void
-emu_chunked_array_apply_v1(void ** array, long n, long grain,
-    void * arg1,
+emu_chunked_array_apply_v1(void ** array, long n, long grain
+    , void * arg1,
     void (*worker)(long begin, long end, void * arg1))
 {
     // Each thread will be responsible for the elements on one nodelet
@@ -71,3 +110,121 @@ emu_chunked_array_apply_v1(void ** array, long n, long grain,
         cilk_spawn emu_chunked_array_apply_v1_level1(array[i], begin, end, grain, arg1, worker);
     }
 }
+
+static noinline void
+emu_chunked_array_apply_v2_level1(void * hint, long begin, long end, long grain
+    , void * arg1, void * arg2,
+    void (*worker)(long begin, long end, void * arg1, void * arg2)
+)
+{
+    (void)hint;
+    for (long i = begin; i < end; i += grain) {
+        long first = i;
+        long last = first + grain; if (last > end) { last = end; }
+        cilk_spawn worker(first, last, arg1, arg2);
+    }
+}
+
+void
+emu_chunked_array_apply_v2(void ** array, long n, long grain
+    , void * arg1, void * arg2,
+    void (*worker)(long begin, long end, void * arg1, void * arg2))
+{
+    // Each thread will be responsible for the elements on one nodelet
+    long local_n = n / NODELETS();
+    // Spawn a thread on each nodelet
+    for (long i = 0; i < NODELETS(); ++i) {
+        long begin = local_n * i;
+        long end = local_n * (i + 1); if (end > n) { end = n; }
+        cilk_spawn emu_chunked_array_apply_v2_level1(array[i], begin, end, grain, arg1, arg2, worker);
+    }
+}
+
+static noinline void
+emu_chunked_array_apply_v3_level1(void * hint, long begin, long end, long grain
+    , void * arg1, void * arg2, void * arg3,
+    void (*worker)(long begin, long end, void * arg1, void * arg2, void * arg3)
+)
+{
+    (void)hint;
+    for (long i = begin; i < end; i += grain) {
+        long first = i;
+        long last = first + grain; if (last > end) { last = end; }
+        cilk_spawn worker(first, last, arg1, arg2, arg3);
+    }
+}
+
+void
+emu_chunked_array_apply_v3(void ** array, long n, long grain
+    , void * arg1, void * arg2, void * arg3,
+    void (*worker)(long begin, long end, void * arg1, void * arg2, void * arg3))
+{
+    // Each thread will be responsible for the elements on one nodelet
+    long local_n = n / NODELETS();
+    // Spawn a thread on each nodelet
+    for (long i = 0; i < NODELETS(); ++i) {
+        long begin = local_n * i;
+        long end = local_n * (i + 1); if (end > n) { end = n; }
+        cilk_spawn emu_chunked_array_apply_v3_level1(array[i], begin, end, grain, arg1, arg2, arg3, worker);
+    }
+}
+
+static noinline void
+emu_chunked_array_apply_v4_level1(void * hint, long begin, long end, long grain
+    , void * arg1, void * arg2, void * arg3, void * arg4,
+    void (*worker)(long begin, long end, void * arg1, void * arg2, void * arg3, void * arg4)
+)
+{
+    (void)hint;
+    for (long i = begin; i < end; i += grain) {
+        long first = i;
+        long last = first + grain; if (last > end) { last = end; }
+        cilk_spawn worker(first, last, arg1, arg2, arg3, arg4);
+    }
+}
+
+void
+emu_chunked_array_apply_v4(void ** array, long n, long grain
+    , void * arg1, void * arg2, void * arg3, void * arg4,
+    void (*worker)(long begin, long end, void * arg1, void * arg2, void * arg3, void * arg4))
+{
+    // Each thread will be responsible for the elements on one nodelet
+    long local_n = n / NODELETS();
+    // Spawn a thread on each nodelet
+    for (long i = 0; i < NODELETS(); ++i) {
+        long begin = local_n * i;
+        long end = local_n * (i + 1); if (end > n) { end = n; }
+        cilk_spawn emu_chunked_array_apply_v4_level1(array[i], begin, end, grain, arg1, arg2, arg3, arg4, worker);
+    }
+}
+
+static noinline void
+emu_chunked_array_apply_v5_level1(void * hint, long begin, long end, long grain
+    , void * arg1, void * arg2, void * arg3, void * arg4, void * arg5,
+    void (*worker)(long begin, long end, void * arg1, void * arg2, void * arg3, void * arg4, void * arg5)
+)
+{
+    (void)hint;
+    for (long i = begin; i < end; i += grain) {
+        long first = i;
+        long last = first + grain; if (last > end) { last = end; }
+        cilk_spawn worker(first, last, arg1, arg2, arg3, arg4, arg5);
+    }
+}
+
+void
+emu_chunked_array_apply_v5(void ** array, long n, long grain
+    , void * arg1, void * arg2, void * arg3, void * arg4, void * arg5,
+    void (*worker)(long begin, long end, void * arg1, void * arg2, void * arg3, void * arg4, void * arg5))
+{
+    // Each thread will be responsible for the elements on one nodelet
+    long local_n = n / NODELETS();
+    // Spawn a thread on each nodelet
+    for (long i = 0; i < NODELETS(); ++i) {
+        long begin = local_n * i;
+        long end = local_n * (i + 1); if (end > n) { end = n; }
+        cilk_spawn emu_chunked_array_apply_v5_level1(array[i], begin, end, grain, arg1, arg2, arg3, arg4, arg5, worker);
+    }
+}
+
+/* [[[end]]] */
--- emu_for_2d.h
+++ emu_for_2d.h
@@ -1,21 +1,57 @@
 #pragma once
 
+/*[[[cog
+
+from string import Template
+
+for num_args in xrange(6):
+
+    arg_decls = "".join([", void * arg%i"%(i+1) for i in xrange(num_args)])
+    declaration = Template("""
+        void
+        emu_chunked_array_apply_v${num_args}(
+            void ** array, long num_elements, long grain
+            ${arg_decls},
+            void (*worker)(long begin, long end${arg_decls})
+        );
+    """)
+    cog.out(declaration.substitute(**locals()), dedent=True, trimblanklines=True)
+
+]]]*/
 void
 emu_chunked_array_apply_v0(
-    void ** array, long num_elements, long grain,
+    void ** array, long num_elements, long grain
+    ,
     void (*worker)(long begin, long end)
 );
-
 void
 emu_chunked_array_apply_v1(
-    void ** array, long num_elements, long grain,
-    void * arg1,
+    void ** array, long num_elements, long grain
+    , void * arg1,
     void (*worker)(long begin, long end, void * arg1)
 );
-
 void
 emu_chunked_array_apply_v2(
-    void ** array, long num_elements, long grain,
-    void * arg1, void * arg2,
+    void ** array, long num_elements, long grain
+    , void * arg1, void * arg2,
     void (*worker)(long begin, long end, void * arg1, void * arg2)
 );
+void
+emu_chunked_array_apply_v3(
+    void ** array, long num_elements, long grain
+    , void * arg1, void * arg2, void * arg3,
+    void (*worker)(long begin, long end, void * arg1, void * arg2, void * arg3)
+);
+void
+emu_chunked_array_apply_v4(
+    void ** array, long num_elements, long grain
+    , void * arg1, void * arg2, void * arg3, void * arg4,
+    void (*worker)(long begin, long end, void * arg1, void * arg2, void * arg3, void * arg4)
+);
+void
+emu_chunked_array_apply_v5(
+    void ** array, long num_elements, long grain
+    , void * arg1, void * arg2, void * arg3, void * arg4, void * arg5,
+    void (*worker)(long begin, long end, void * arg1, void * arg2, void * arg3, void * arg4, void * arg5)
+);
+/* [[[end]]] */
