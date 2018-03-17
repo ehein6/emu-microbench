42ef371ca59d7338ce928164a6a1cc59ab8e5860
Eric Hein <ehein6@gatech.edu>
2018-03-17 13:55:47 -0400

    New grain size helpers with minimum grain
    These functions were indended to limit the number of threads
    spawned to avoid overwhelming the hardware, but we lost the
    ability to spawn fewer threads for a small array. The new
    functions allow the user to specify a miniumum grain size.

--- emu_c_utils/emu_grain_helpers.h
+++ emu_c_utils/emu_grain_helpers.h
@@ -15,6 +15,12 @@
 
 long get_gcs_per_nodelet();
 
+/**
+ * Calculates the grain size for a loop of @c n iterations that will spawn the maximum number of threads across the
+ * entire system.
+ * @param n number of loop iterations
+ * @return grain size
+ */
 static inline long
 GLOBAL_GRAIN(long n)
 {
@@ -22,9 +28,40 @@ GLOBAL_GRAIN(long n)
     return n > global_num_threads ? (n/global_num_threads) : 1;
 }
 
+/**
+ * Calculates the grain size for a loop of @c n iterations that will spawn the maximum number of threads on one nodelet
+ * @param n number of loop iterations
+ * @return grain size
+ */
 static inline long
 LOCAL_GRAIN(long n)
 {
     long local_num_threads = THREADS_PER_GC * get_gcs_per_nodelet();
     return n > local_num_threads ? (n/local_num_threads) : 1;
+}
+
+/**
+ * Similar to LOCAL_GRAIN, except the final grain size can never be smaller than @c min_grain
+ * @param n number of loop iterations
+ * @param min_grain minimum grain size to return
+ * @return grain size
+ */
+static inline long
+LOCAL_GRAIN_MIN(long n, long min_grain)
+{
+    long grain = LOCAL_GRAIN(n);
+    return grain > min_grain ? grain : min_grain;
+}
+
+/**
+ * Similar to GLOBAL_GRAIN, except the final grain size can never be smaller than @c min_grain
+ * @param n number of loop iterations
+ * @param min_grain minimum grain size to return
+ * @return grain size
+ */
+static inline long
+GLOBAL_GRAIN_MIN(long n, long min_grain)
+{
+    long grain = GLOBAL_GRAIN(n);
+    return grain > min_grain ? grain : min_grain;
 }
