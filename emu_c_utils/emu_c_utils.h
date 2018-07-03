1c74b6bbb5d680010631b25bb1c405ce59de1569
Eric Hein <ehein6@gatech.edu>
2018-07-03 16:58:24 -0400

    New routine to run a function on each nodelet

--- emu_c_utils/emu_c_utils.h
+++ emu_c_utils/emu_c_utils.h
@@ -6,6 +6,28 @@
 #pragma once
 
 #include <stddef.h>
+#include <stdarg.h>
 
 #define REPLICATE(X) replicate_struct((X), sizeof(*(X)))
 void replicate_struct(void * s, size_t sz);
+
+/**
+ * Execute a function on each nodelet in parallel
+ * @param array Pointer to any replicated variable
+ * @param worker Function that will be executed on each nodelet.
+ * @param ...
+ */
+void emu_for_each_nodelet(
+    long * array,
+    void (*worker)(void * hint, long nodelet_id, va_list args),
+    ...
+);
+
+/**
+ * Like emu_for_each_nodelet, but accepts a va_list to allow forwarding of varargs.
+ */
+void emu_for_each_nodelet_var(
+    long * array,
+    void (*worker)(void * hint, long nodelet_id, va_list args),
+    va_list args
+);
