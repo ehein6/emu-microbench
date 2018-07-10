a0ebf4f6fd9374c07d97a9903886970f6d8febd3
Eric Hein <ehein6@gatech.edu>
2018-07-10 17:52:20 -0400

    Added tree-based scatter for replicated array init

--- emu_c_utils/emu_scatter_gather.c
+++ emu_c_utils/emu_scatter_gather.c
@@ -6,6 +6,7 @@
 #include "emu_scatter_gather.h"
 #include <string.h>
 #include <cilk/cilk.h>
+#include "emu_for_local.h"
 
 #if defined(__le64__)
 #include <memoryweb.h>
@@ -52,4 +53,34 @@ emu_chunked_array_from_local(emu_chunked_array * self, void * local_array)
         long * dst_array = (long*)self->data[i];
         cilk_spawn memcpy_long(dst_array, src_array, longs_per_chunk);
     }
+}
+
+
+static void
+scatter_tree(void * buffer, long n, long nlet_begin, long nlet_end)
+{
+    long num_nodelets = nlet_end - nlet_begin;
+    // TODO detect this condition before spawning
+    if (num_nodelets == 1) { return; }
+
+    long nlet_mid = nlet_begin + (num_nodelets / 2);
+
+    long * local = mw_get_nth(buffer, NODE_ID());
+    long * remote = mw_get_nth(buffer, nlet_mid);
+
+//    LOG("nlet[%li]: Copy from %li to %li\n", NODE_ID(), NODE_ID(), nlet_mid);
+    // Copy to nlet_mid
+    emu_local_for_copy_long(remote, local, n);
+
+//    LOG("nlet[%li]: Spawn scatter_tree(%li - %li)\n", NODE_ID(), nlet_mid, nlet_end);
+
+    // Spawn at target and recurse through my range
+    cilk_spawn scatter_tree(remote, n, nlet_mid, nlet_end);
+    scatter_tree(local, n, nlet_begin, nlet_mid);
+}
+
+void
+emu_replicated_array_init(long * array, long n)
+{
+    scatter_tree(array, n, 0, NODELETS());
 }
